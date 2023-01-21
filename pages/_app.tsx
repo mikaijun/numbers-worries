40c1bee2ce49f0ccdaf0760fdee5d6e1b705a95d
mikaijun <mikai@anyinc.jp>
2023-01-21 16:04:51 +0900

    ApolloClientの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import type { AppProps } from "next/app"
 import { RecoilRoot } from "recoil"
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: '/api/graphql',
+  cache: new InMemoryCache(),
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <ApolloProvider client={client}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </ApolloProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,34 @@ import { useSessionStorage } from "@/hooks/useSessionStorage"
 
 import type { NextPage } from "next"
 
+import { gql, useQuery } from '@apollo/client';
+
+const GET_USERS = gql`
+query GetUsers {
+  users {
+    id
+    name
+    email
+  }
+}
+`;
+
 const Page: NextPage = () => {
   const { worries } = useSessionStorage()
+  const { data, loading, error } = useQuery(GET_USERS);
+  if (loading) return <p>ローディング中です</p>;
+  if (error) return <p>エラーが発生しています</p>;
+
+  const { users } = data;
 
   return (
     <div>
+      <main>
+        <h1>ユーザ情報</h1>
+        {users.map((user: { id: number; name: string; email: string }) => (
+          <div key={user.id}>Name: {user.name}</div>
+        ))}
+      </main>
       {worries.map((worry, index) => (
         <div key={index}>
           <Link href={`/${worry.id}`}>
