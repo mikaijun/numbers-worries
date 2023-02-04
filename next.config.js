d90065f2a215e727ac9678238346148a307bd038
mikaijun <mikai@anyinc.jp>
2023-02-05 07:47:03 +0900

    vercel.jsonの設定変更

--- next.config.js
+++ next.config.js
@@ -2,8 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
-    emotion: true
-  }
+    emotion: true,
+  },
+  webpack: (config, { webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.plugins.push(new webpack.IgnorePlugin(/\.stor(ies|y).[tj]sx$/))
+    return config
+  },
 }
 
 module.exports = nextConfig
