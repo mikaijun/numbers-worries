bd8de4200dfd82e7a13c6063ca3533a4f77dd723
mikaijun <mikai@anyinc.jp>
2023-02-05 07:59:53 +0900

    デプロイとリダイレクト両方やってみる

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
+  // via. https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+  webpack: (config, { webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.plugins.push(new webpack.IgnorePlugin(/\.stor(ies|y).[tj]sx$/))
+    return config
+  },
 }
 
 module.exports = nextConfig
