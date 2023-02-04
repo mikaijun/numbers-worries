7a39c333813d7a4dab6b0b57ad5aee8538db891d
mikaijun <mikai@anyinc.jp>
2023-02-05 08:36:11 +0900

    リダイレクト設定

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
+  webpack: (config, { webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.plugins.push(new webpack.IgnorePlugin(/\.stor(ies|y).[tj]sx$/))
+    return config
+  },
 }
 
 module.exports = nextConfig
