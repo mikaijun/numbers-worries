9903bb3d94f58e5742ac0e34fc4efb013bd0e78f
mikaijun <mikai@anyinc.jp>
2023-02-05 16:39:00 +0900

    入力フォームのstoriesを一旦コメント

--- components/organisms/WorryForm/stories.tsx
+++ components/organisms/WorryForm/stories.tsx
@@ -1,29 +1,29 @@
-import WorryForm from "./index"
+// import WorryForm from "./index"
 
-import type { ComponentMeta, ComponentStoryObj } from "@storybook/react"
+// import type { ComponentMeta, ComponentStoryObj } from "@storybook/react"
 
-export default {
-  title: "Organisms/Worry/WorryForm",
-  component: WorryForm,
-  argTypes: {
-    onClose: { action: "onClose" },
-  },
-  decorators: [
-    (Story) => {
-      return <Story />
-    },
-  ],
-} as ComponentMeta<typeof WorryForm>
+// export default {
+//   title: "Organisms/Worry/WorryForm",
+//   component: WorryForm,
+//   argTypes: {
+//     onClose: { action: "onClose" },
+//   },
+//   decorators: [
+//     (Story) => {
+//       return <Story />
+//     },
+//   ],
+// } as ComponentMeta<typeof WorryForm>
 
-export const Primary: ComponentStoryObj<typeof WorryForm> = {
-  args: {
-    worry: {
-      content: "心配事1",
-      damage_rate: 0,
-      id: 1,
-      reality_events: "現実1",
-      suppose_maximum_events: "最大値1",
-      suppose_minimum_events: "最小値1",
-    },
-  },
-}
+// export const Primary: ComponentStoryObj<typeof WorryForm> = {
+//   args: {
+//     worry: {
+//       content: "心配事1",
+//       damage_rate: 0,
+//       id: 1,
+//       reality_events: "現実1",
+//       suppose_maximum_events: "最大値1",
+//       suppose_minimum_events: "最小値1",
+//     },
+//   },
+// }
